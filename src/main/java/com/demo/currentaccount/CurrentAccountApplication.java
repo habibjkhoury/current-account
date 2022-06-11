ee5ce7b6f15ad8aaedb7ba712e1f1984c8e8371c
Habib Khoury <habib.khoury@foo-solutions.com>
2022-06-11 19:23:05 +0300

    Initial Commit

---
+++ src/main/java/com/demo/currentaccount/CurrentAccountApplication.java
@@ -0,0 +1,13 @@
+package com.demo.currentaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrentAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/currentaccount/CurrentAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.currentaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
